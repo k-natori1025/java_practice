518b6235cc802fbe206a54056d95830b685235be
k-natori1025 <k-natori1025@gmail.com>
2025-02-16 18:23:16 +0900

    インターフェース

---
+++ src/_09/company/ExtPrintable.java
@@ -0,0 +1,11 @@
+package _09.company;
+
+/**
+ * 外部帳票の印刷に対応しているインターフェース
+ */
+public interface ExtPrintable {
+    /**
+     * 印刷処理
+     */
+    void print();
+}
---
+++ src/_09/company/Shippable.java
@@ -0,0 +1,11 @@
+package _09.company;
+
+/**
+ * 出荷に対応しているインターフェース
+ */
+public interface Shippable {
+    /**
+     * 出荷処理
+     */
+    void ship();
+}
